d6728f410030b8efd7dac9e0d22091c75a1e9b28
Remmington Pascone <remmingtonpearce@gmail.com>
2021-01-03 20:27:11 -0800

    added CSS classes based on time

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,6 @@ $(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do "))
 
 
-
-
     // determine current hour
     // get current number of hours
     var currentHour = moment().hours();
@@ -19,35 +17,43 @@ $(document).ready(function () {
     console.log(timeBlock.length);
 
     // forEach loop over hour blocks
-    $(".time-block").each(function( ) {
-    // get value from html for current hour 
-    var blockHour = parseInt($(this).attr("id").split("-")[1]);
-    console.log(blockHour);
+    $(".time-block").each(function () {
+        // get value from html for current hour 
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        console.log(blockHour);
 
-    // change div timeblock colors depending on time of day
-    // check if currentHour > or < blockHour
-    // add css classes according to time
-    if (currentHour > blockHour) {
-        console.log("div is grey"); 
-        
-    }
-    else if (currentHour < blockHour) {
-        console.log("div is green"); 
-    }
 
-    else {
-        console.log("div is red"); 
+        // change div timeblock colors depending on time of day
+        // check if currentHour > or < blockHour
+        // add css classes according to time
+        if (currentHour > blockHour) {
+            // testing that my loop is working
+            console.log("div is grey");
+            $(this).addClass("past");
+
+        }
+        else if (currentHour < blockHour) {
+            // testing that my loop is working
+            console.log("div is green");
+            $(this).addClass("future");
+        }
+
+        else {
+            // testing that my loop is working
+            console.log("div is red");
+            $(this).addClass("present");
+
         }
 
     });
 
 
-    
-  
 
 
 
-    
+
+
+
 
 
 
