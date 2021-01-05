2881d04c2ba8e8e7cbe3ef811f272f26f8ce765b
Remmington Pascone <remmingtonpearce@gmail.com>
2021-01-05 15:43:31 -0800

    grab values from local storage and display in blocks

--- script.js
+++ script.js
@@ -54,19 +54,36 @@ $(document).ready(function () {
     // add to local storage
     // render from local storage on refresh
     
-     
-
+    
+    
     // saves data to be used in localStorage
     $(".saveBtn").on("click", function(event) {
     event.preventDefault();
     
     var text = $(this).siblings(".description").val(); 
-    var time = $(this).parent().attr("id"); 
-
-    localStorage.setItem(time, text); 
+    var time = $(this).parent().attr("id");
     
+     
+    localStorage.setItem(time, text);
     
     }); 
+
+    // get everything from local storage and update page 
+    
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+    
+
+    
 
 });
     
